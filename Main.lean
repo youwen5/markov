921f1a5d18f12c8cc790c634c67cdcf485b0327a
Youwen Wu <youwenw@gmail.com>
2026-01-30 01:22:52 -0800

    feat: add some parser logic

--- Main.lean
+++ Main.lean
@@ -1,4 +1,30 @@
+import Lean
+import LeanSearchClient
 import Markov
+import Lean.Data.Json.Parser
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+structure Content where
+  body : String
+  msgtype : String
+  deriving Lean.ToJson, Lean.FromJson, Inhabited, Repr
+
+structure Messages where
+  content : Content
+  type : String
+  deriving Lean.ToJson, Lean.FromJson, Inhabited, Repr
+
+structure Matrix where
+  room_name : String
+  messages : List Messages
+  deriving Lean.ToJson, Lean.FromJson, Inhabited, Repr
+
+def main : IO Unit := do
+  let data ← IO.FS.readFile "matrix.json"
+  match Lean.Json.parse data with
+  | Except.error e => IO.println s!"Error occurred: {e}"
+  | Except.ok a => 
+    let validated : Except String Matrix := Lean.FromJson.fromJson? a
+    if let Except.error e := validated then do
+      IO.println s!"Error validating JSON: {e}"
+      return
+  IO.println <| String.take data 50
